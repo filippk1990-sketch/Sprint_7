7213b26e65ddfbfec6161173fb0f3a5d98c4e06a
Filipp Kroschenko <k.filipp90@yandex.ru>
2026-09-25 19:06:41 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,11 @@
+# Базовый шаблон заказа
+ORDER_PAYLOAD = {
+    "firstName": "Иван",
+    "lastName": "Иванов",
+    "address": "Москва, ул. Ленина, 1",
+    "metroStation": 4,
+    "phone": "+79991112233",
+    "rentTime": 3,
+    "deliveryDate": "2026-10-10",
+    "comment": "Позвонить за час"
+}
